a15d0391011ca1ed324e0f761bd296b2582027f6
ElizaSuh <108683493+ElizaSuh@users.noreply.github.com>
2022-08-11 15:51:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 var root = document.querySelector('#root');
-var button = root.querySelector("#root button")
+if (root.querySelector("#root button")) {
+	var button = root.querySelector("#root button");
+}
+if (root.querySelector(".product-in-cart")) {
+	var productInCart = root.querySelector(".product-in-cart")
+}
 
-button.addEventListener("click", () => {
-	if (checkStorageForItem() === null) {
-		localStorage.setItem('2053266', JSON.stringify(buildProductJson()));
-	} else {
-		var item = JSON.parse(localStorage.getItem('2053266'));
-		item.qty++
-		localStorage.setItem('2053266', JSON.stringify(item));
-	}
-	
+if (productInCart) {
+	var name = productInCart.querySelector(".name");
+	var price = productInCart.querySelector(".price");
+	var quantity = productInCart.querySelector(".quantity");
+	fillCartData(name, price, quantity);
+}
 
-})
+if (button.classList.contains("checkout")) {
+	button.addEventListener("click", () => {
+		localStorage.removeItem('2053266');
+		window.location.href="file:///Users/e.suhareva/Dev/demo-page/index.html";
+	})
+} else {
+	button.addEventListener("click", () => {
+		if (checkStorageForItem() === null) {
+			localStorage.setItem('2053266', JSON.stringify(buildProductJson()));
+		} else {
+			var item = JSON.parse(localStorage.getItem('2053266'));
+			item.qty++
+			localStorage.setItem('2053266', JSON.stringify(item));
+		}
+
+
+	})
+}
 
 function buildProductJson(){
 	return {
@@ -23,4 +42,11 @@ function buildProductJson(){
 
 function checkStorageForItem(){
 	return localStorage.getItem('2053266');
+}
+
+function fillCartData(name, price){
+	var item = JSON.parse(localStorage.getItem('2053266'));
+	name.textContent = item.name;
+	price.textContent = item.price;
+	quantity.textContent = item.qty;
 }
