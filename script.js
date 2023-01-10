713c95558536c86697c5dea062e8eb25432fbbc9
ElizaSuh <108683493+ElizaSuh@users.noreply.github.com>
2023-01-10 15:44:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ var root = document.querySelector('#root');
 if (root.querySelector("#root button")) {
 	var button = root.querySelector("#root button");
 }
-if (root.querySelector(".product-in-cart")) {
-	var productInCart = root.querySelector(".product-in-cart")
+if (root.querySelector(".product-in-cart.item")) {
+	var productInCart = root.querySelector(".product-in-cart.item")
+}
+if (root.querySelector(".product-in-cart.no-items-found")) {
+	var emptyCart = root.querySelector(".product-in-cart.no-items-found")
 }
 
-if (productInCart) {
+if (checkStorageForItem) {
+	var image = productInCart.querySelector("img");
 	var name = productInCart.querySelector(".name");
 	var price = productInCart.querySelector(".price");
 	var quantity = productInCart.querySelector(".quantity");
-	fillCartData(name, price, quantity);
+	fillCartData(image, name, price, quantity);
+	productInCart.style.setProperty("display", "flex")
+	emptyCart.style.setProperty("display", "none")
 }
 
 if (button.classList.contains("checkout")) {
@@ -34,6 +40,7 @@ if (button.classList.contains("checkout")) {
 
 function buildProductJson(){
 	return {
+		"image": "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
 		"name": "EOS 500D body",
 		"price": "1695.36",
 		"qty" : 1
@@ -44,9 +51,12 @@ function checkStorageForItem(){
 	return localStorage.getItem('2053266');
 }
 
-function fillCartData(name, price){
+function fillCartData(image, name, price, quantity){
 	var item = JSON.parse(localStorage.getItem('2053266'));
-	name.textContent = item.name;
-	price.textContent = item.price;
-	quantity.textContent = item.qty;
+	if(item) {
+		image.src = item.image;
+		name.textContent = item.name;
+		price.textContent = item.price;
+		quantity.textContent = item.qty;
+	}
 }
