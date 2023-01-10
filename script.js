58caf403e97954517e4c045285d6c6af33013db8
ElizaSuh <108683493+ElizaSuh@users.noreply.github.com>
2023-01-10 15:47:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ if (button.classList.contains("checkout")) {
 
 function buildProductJson(){
 	return {
-		"image": "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+		"image": "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
 		"name": "EOS 500D body",
 		"price": "1695.36",
 		"qty" : 1
