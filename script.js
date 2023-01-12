b452a6029d314c43187602cb18c83d132066d75c
ElizaSuh <108683493+ElizaSuh@users.noreply.github.com>
2023-01-12 13:26:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var root = document.querySelector('#root');
-if (root.querySelector("#root button")) {
+
+// cart page logic
+if (window.location.href.includes("cart")) {
 	var button = root.querySelector("#root button");
-}
-if (root.querySelector(".product-in-cart.item")) {
 	var productInCart = root.querySelector(".product-in-cart.item")
-}
-if (root.querySelector(".product-in-cart.no-items-found")) {
 	var emptyCart = root.querySelector(".product-in-cart.no-items-found")
-}
-
-if (checkStorageForItem()) {
-	fillCartData();
-	emptyCart.style.setProperty("display", "none")
-}
-
-if (button.classList.contains("checkout")) {
+	
+// 	fill cart with stored data
+	if (checkStorageForItem()) {
+		fillCartData();
+		emptyCart.style.setProperty("display", "none")
+	}
+	
+// 	clear cart after checkout
 	button.addEventListener("click", () => {
 		localStorage.removeItem('cart');
 		window.location.href="./index.html";
 	})
-} else {
+} 
+
+// product page logic
+else if (window.location.href.includes("product")){
+	var button = root.querySelector("#root button");
 	var sku = button.getAttribute("sku");
+	
 	button.addEventListener("click", () => {
 		if (checkStorageForItem() === null) {
 			var cart = {};
@@ -35,6 +38,33 @@ if (button.classList.contains("checkout")) {
 
 	})
 }
+
+// if (window.location.href.includes("cart") && checkStorageForItem()) {
+// 	fillCartData();
+// 	emptyCart.style.setProperty("display", "none")
+// }
+
+// if (button.classList.contains("checkout")) {
+// 	button.addEventListener("click", () => {
+// 		localStorage.removeItem('cart');
+// 		window.location.href="./index.html";
+// 	})
+// } else {
+// 	var sku = button.getAttribute("sku");
+// 	button.addEventListener("click", () => {
+// 		if (checkStorageForItem() === null) {
+// 			var cart = {};
+// 			cart[sku] = buildProductJson(button)
+// 			localStorage.setItem('cart', JSON.stringify(cart));
+// 		} else {
+// 			var cart = JSON.parse(localStorage.getItem('cart'));
+// 			cart[sku] ? cart[sku].qty++ : cart[sku] = buildProductJson(button);
+// 			localStorage.setItem('cart', JSON.stringify(cart));
+// 		}
+
+
+// 	})
+// }
 
 function buildProductJson(el){
 	return {
